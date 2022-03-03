c5b4017240362a879af31aa14ad115e77b1ae2b3
jonathan-cederquist <97691358+jonathan-cederquist@users.noreply.github.com>
2022-03-03 11:39:56 -0800

    Able to spin 3 motors and read encoders

--- src/RoboEncoderDriver.py
+++ src/RoboEncoderDriver.py
@@ -1,19 +1,20 @@
-''' @file       EncoderDriver.py
-    @brief      Used to read from encoders attached to motors
+''' @file       RoboEncoderDriver.py
+    @brief      Used to read from encoders attached to motors (No. GB37Y3530-12V-83R)
     @details    Utilizes alternate functions from timer channels on pyb board to 
                 read from attached encoders
     @author     Jonathan Cederquist
     @author     Tim Jain
     @author     Philip Pang
-    @date       Last Modified 1/26/22
+    @date       Last Modified 3/3/22
 '''
 
 import pyb
 import utime
+import RoboMotorDriver
 
-class EncoderDriver:
+class RoboEncoderDriver:
     '''! 
-    This class implements an encoder driver for an ME405 kit. 
+    This class implements an encoder driver for an ME 405 Robot. 
     '''
     
     def __init__ (self, in1pin, in2pin, timer):
@@ -63,8 +64,8 @@ class EncoderDriver:
         @details    Converts the current encoder position reading (in ticks)
                     to radians using the encoder CPR and gear ratio
         '''
-        gearRatio = 16
-        CPR = 256
+        gearRatio = 131
+        CPR = 16
         
         return self.current_position * 3.1415926535 / (gearRatio*CPR)
     
@@ -81,14 +82,56 @@ class EncoderDriver:
         
 if __name__ == "__main__":
     
-    encoder2 = EncoderDriver(pyb.Pin(pyb.Pin.cpu.C6), pyb.Pin(pyb.Pin.cpu.C7), 8)
-    time = utime.ticks_ms()
+    encoder1 = RoboEncoderDriver(pyb.Pin(pyb.Pin.board.PB6), pyb.Pin(pyb.Pin.board.PB7), 4)
+    encoder2 = RoboEncoderDriver(pyb.Pin(pyb.Pin.board.PC6), pyb.Pin(pyb.Pin.board.PC7), 8)
+    encoder3 = RoboEncoderDriver(pyb.Pin(pyb.Pin.board.PA0), pyb.Pin(pyb.Pin.board.PA1), 5)
     
+    
+    # Instantiates three motor driver objects with appropriate pins and timer number
+    pinB5 = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
+    pinB3 = pyb.Pin(pyb.Pin.board.PB3, pyb.Pin.OUT_PP)
+    
+    motor1 = RoboMotorDriver.RoboMotorDriver(pinB5, pinB3, 2, 2)
+    
+    # Motor 2
+    pinA6 = pyb.Pin(pyb.Pin.board.PA6, pyb.Pin.OUT_PP)
+    pinA7 = pyb.Pin(pyb.Pin.board.PA7, pyb.Pin.OUT_PP)
+    
+    motor2 = RoboMotorDriver.RoboMotorDriver(pinA6, pinA7, 3, 2)
+    
+    # Motor 3
+    pinA9 = pyb.Pin(pyb.Pin.board.PA9, pyb.Pin.OUT_PP)
+    pinB4 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+    
+    motor3 = RoboMotorDriver.RoboMotorDriver(pinA9, pinB4, 3, 1)
+    
+    # Start motor 1, 2, 3
+    motor1.set_duty_cycle(25)
+    motor2.set_duty_cycle(25)
+    motor3.set_duty_cycle(25)
+    
+    time = utime.ticks_ms()
     
     #while utime.ticks_ms() < (time + 10000):
     while True:
+        encoder1.update()
         encoder2.update()
-        if (utime.ticks_ms() > (time + 500)):
-            print("\ntimer counter:", encoder2.timer.counter())
-            print("\nencoder driver", encoder2.read())
-            time += 500
+        encoder3.update()
+        
+        if (utime.ticks_ms() > (time + 50)):
+            print("\ntimer counter1:", encoder1.timer.counter())
+            print("\nencoder driver1", encoder1.read())
+            
+            print("\ntimer counter2:", encoder2.timer.counter())
+            print("\nencoder driver2", encoder2.read())
+            
+            print("\ntimer counter3:", encoder3.timer.counter())
+            print("\nencoder driver3", encoder3.read())
+            
+            if encoder1.read() > 6.28:
+                break
+            time += 50
+    
+    motor1.set_duty_cycle(0)
+    motor2.set_duty_cycle(0)
+    motor3.set_duty_cycle(0)
--- src/RoboMotorDriver.py
+++ src/RoboMotorDriver.py
@@ -19,14 +19,15 @@ class RoboMotorDriver:
     @brief This class allows instances of motors to be made and controlled individually
     '''
 
-    def __init__ (self, dir_Pin, speed_Pin, timer_Num):
+    def __init__ (self, dir_Pin, speed_Pin, timer_Num, channel_Num):
         '''! 
         @brief Instantiates motor driver by initializing GPIO pins and turning the motor off for safety. 
         @details A timer object is also made. This will be used to send PWM'd power to the motors
         
-        @param dir_Pin   Pin object for setting motor direction: low() for fwd, high() for rev
-        @param speed_Pin Pin object for use with timer PWM channel to set motor speed
-        @param timer     An integer designating which timer to use for the pin objects specified
+        @param dir_Pin       Pin object for setting motor direction: low() for fwd, high() for rev
+        @param speed_Pin     Pin object for use with timer PWM channel to set motor speed
+        @param timer_Num     An integer designating which timer to use for the pin objects specified
+        @param channel_Num   An integer representing the channel used with the motor timer
         '''
         print ('Creating a robo motor driver')
         
@@ -36,7 +37,7 @@ class RoboMotorDriver:
         
         # a timer object is created with pins configured to create PWM signals
         tim = pyb.Timer (timer_Num, freq=20000)
-        self.ch2 = tim.channel (2, pyb.Timer.PWM, pin=self.speedPin)
+        self.ch = tim.channel(channel_Num, pyb.Timer.PWM, pin=self.speedPin)
         
 
     def set_duty_cycle (self, duty):
@@ -51,65 +52,94 @@ class RoboMotorDriver:
             self.dirPin.low()
             # CASE 1: duty cycle needs to be at MAX = 100%
             if duty > 100:
-                self.ch2.pulse_width_percent(100)
+                self.ch.pulse_width_percent(100)
             # CASE 2: duty cycle is below MAX = 100%
             else:
-                self.ch2.pulse_width_percent(duty)  
+                self.ch.pulse_width_percent(duty)  
         
         # CASE 3 & 4: negative duty cycle
         elif duty < 0:
             self.dirPin.high()
             # CASE 3: duty cycle needs to be at MAX = 100%
             if duty < -100:
-                self.ch2.pulse_width_percent(100)
+                self.ch.pulse_width_percent(100)
             # CASE 4: duty cycle is below MAX = 100%
             else:
-                self.ch2.pulse_width_percent(-duty)
+                self.ch.pulse_width_percent(-duty)
         
         # CASE 5: duty cycle is zero
         else:
-            self.ch2.pulse_width_percent(0)
+            self.ch.pulse_width_percent(0)
 
         
     def enable(self):
         '''!
-        @brief This method enables the motor by setting the sleep pin to high
+        @brief This method should never be used
         '''
         pass
         
     def disable(self):
         '''!
-        @brief This method disables the motor by setting the sleep pin to low
+        @brief This method should never be used
         '''
-        self.ch2.pulse_width_percent(0)
+        pass
         
 if __name__ == "__main__":
     '''!
     @brief Tests MotorDriver.py by creating two MotorDriver objects and sets them to different speeds
     '''
-    # Instantiates two motor driver objects with appropriate pins and timer number
+    # Instantiates three motor driver objects with appropriate pins and timer number
     pinB5 = pyb.Pin(pyb.Pin.board.PB5, pyb.Pin.OUT_PP)
     pinB3 = pyb.Pin(pyb.Pin.board.PB3, pyb.Pin.OUT_PP)
     
-    motor1 = RoboMotorDriver(pinB5, pinB3, 2)
+    motor1 = RoboMotorDriver(pinB5, pinB3, 2, 2)
+    
+    # Motor 2
+    pinA6 = pyb.Pin(pyb.Pin.board.PA6, pyb.Pin.OUT_PP)
+    pinA7 = pyb.Pin(pyb.Pin.board.PA7, pyb.Pin.OUT_PP)
+    
+    motor2 = RoboMotorDriver(pinA6, pinA7, 3, 2)
+    
+    # Motor 3
+    pinA9 = pyb.Pin(pyb.Pin.board.PA9, pyb.Pin.OUT_PP)
+    pinB4 = pyb.Pin(pyb.Pin.board.PB4, pyb.Pin.OUT_PP)
+    
+    motor3 = RoboMotorDriver(pinA9, pinB4, 3, 1)
+    
+    # Motor 4
+    pinA8 = pyb.Pin(pyb.Pin.board.PA8, pyb.Pin.OUT_PP)
+    pinB10 = pyb.Pin(pyb.Pin.board.PB10, pyb.Pin.OUT_PP)
+    
+    motor4 = RoboMotorDriver(pinA8, pinB10, 2, 3)
     
     # Enable motor 1 and 2
-    motor1.set_duty_cycle(50)
+    motor1.set_duty_cycle(100)
+    motor2.set_duty_cycle(100)
+    motor3.set_duty_cycle(100)
+    motor4.set_duty_cycle(100)
     # Stay at current state for 3 seconds
     utime.sleep(3)
     
     # Flip motor direction
-    motor1.set_duty_cycle(-50)
+    motor1.set_duty_cycle(25)
+    motor2.set_duty_cycle(25)
+    motor3.set_duty_cycle(25)
+    motor4.set_duty_cycle(25)
     # Stay at current state for 3 seconds
     utime.sleep(3)
     
     # Test invalid duty cycle
     motor1.set_duty_cycle(-150)
+    motor2.set_duty_cycle(-150)
+    motor3.set_duty_cycle(-150)
+    motor4.set_duty_cycle(-150)
     # Stay at current state for 3 seconds
     utime.sleep(3)
     
     # Stop motors
-    motor1.set_duty_cycle(10)
-    motor1.disable()
-
+    motor1.set_duty_cycle(0)
+    motor2.set_duty_cycle(0)
+    motor3.set_duty_cycle(0)
+    motor4.set_duty_cycle(0)
+    
     
