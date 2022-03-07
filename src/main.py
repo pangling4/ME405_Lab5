918d1de428645d9959b5a4907e896c2cd2a3e53c
jonathan-cederquist <97691358+jonathan-cederquist@users.noreply.github.com>
2022-03-06 16:45:25 -0800

    Work in progress of robot multitasking

--- src/main.py
+++ src/main.py
@@ -16,9 +16,7 @@ import utime
 import cotask
 import task_share
 
-import TaskMotor
-import TaskEncoder
-import TaskController
+import JointTask
 import TaskTouch
         
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
@@ -26,72 +24,51 @@ import TaskTouch
 # printouts show diagnostic information about the tasks, share, and queue.
 if __name__ == "__main__":    
     
-    # Create a shares for motor duty cycle, controller setpoint, and encoder position
-    share_duty_1 = task_share.Share ('i', thread_protect = False, name = "share_duty_1")
-    share_setpoint_1 = task_share.Share ('f', thread_protect = False, name = "share_setpoint_1")
-    share_pos_1 = task_share.Share ('f', thread_protect = False, name = "share_pos_1")
-    queue_pos_1 = task_share.Queue('f', 100, thread_protect = False, name = "queue_pos_1")
-    queue_enc1Times = task_share.Queue('f', 100, thread_protect = False, name = "queue_enc1Times")
+    # Create queues for x and y touchpad positions
+    touchpad_x = task_share.Queue('f', 100, thread_protect = False, name = "touchpad_x")
+    touchpad_y = task_share.Queue('f', 100, thread_protect = False, name = "touchpad_y")
     
-    # Create a shares for second motor duty cycle, controller setpoint, and encoder position
-    share_duty_2 = task_share.Share ('i', thread_protect = False, name = "share_duty_2")
-    share_setpoint_2 = task_share.Share ('f', thread_protect = False, name = "share_setpoint_2")
-    share_pos_2 = task_share.Share ('f', thread_protect = False, name = "share_pos_2")
-    queue_pos_2 = task_share.Queue('f', 100, thread_protect = False, name = "queue_pos_2")
-    queue_enc2Times = task_share.Queue('f', 100, thread_protect = False, name = "queue_enc2Times")
+    # Create queues for joint positions
+    theta_1 = task_share.Queue('f', 100, thread_protect = False, name = "theta_1")
+    theta_2 = task_share.Queue('f', 100, thread_protect = False, name = "theta_2")
+    theta_3 = task_share.Queue('f', 100, thread_protect = False, name = "theta_3")
     
     # Create share flags to control states in tasks
-    share_StartTime_1 = task_share.Share('i', thread_protect = False, name = "share_StartTime")
-    share_Stop_1 = task_share.Share('i', thread_protect = False, name = "share_Stop")
-    share_StartTime_2 = task_share.Share('i', thread_protect = False, name = "share_StartTime_2")
-    share_Stop_2 = task_share.Share('i', thread_protect = False, name = "share_Stop_2")
+#     share_StartTime_1 = task_share.Share('i', thread_protect = False, name = "share_StartTime")
+#     share_Stop_1 = task_share.Share('i', thread_protect = False, name = "share_Stop")
+#     share_StartTime_2 = task_share.Share('i', thread_protect = False, name = "share_StartTime_2")
+#     share_Stop_2 = task_share.Share('i', thread_protect = False, name = "share_Stop_2")
     
     # Initialize shared variables
-    share_duty_1.put(0)
-    share_setpoint_1.put(math.pi*2)
-    share_pos_1.put(0)
-    share_duty_2.put(0)
-    share_setpoint_2.put(math.pi*4)
-    share_pos_2.put(0)
-    share_Stop_1.put(0)
-    share_Stop_2.put(0)
+    # Will be done in the S0_INIT state of the RoboBrain FSM
     
     # Create task objects
-    motor1 = TaskMotor.TaskMotor(1, share_duty_1)
-    enco1 = TaskEncoder.TaskEncoder(1, share_pos_1, share_Stop_1)
-    control1 = TaskController.TaskController(share_setpoint_1, share_duty_1, share_pos_1, share_Stop_1, share_StartTime_1, queue_pos_1, queue_enc1Times)
+    Joint1 = JointTask.JointTask(1, 1, 0.7, 0.05, 0, theta_1)
+    Joint2 = JointTask.JointTask(2, 2, 0.7, 0.05, 0, theta_2)
+    Joint3 = JointTask.JointTask(3, 3, 0.7, 0.05, 0, theta_3)
+    #Brain = RoboTask.RoboTask()
+    # Need to create RoboTask object with run() method which contains overall FSM controlling
+    # behavior of robot
     
-    motor2 = TaskMotor.TaskMotor(2, share_duty_2)
-    enco2 = TaskEncoder.TaskEncoder(2, share_pos_2, share_Stop_2)
-    control2 = TaskController.TaskController(share_setpoint_2, share_duty_2, share_pos_2, share_Stop_2, share_StartTime_2, queue_pos_2, queue_enc2Times)
-
     
     # Putting task objects in cotask run list
-    task1_Mot = cotask.Task (motor1.task, name = 'Task1_Motor', priority = 1,
-                             period = 20, profile = True, trace = False)
+    task1_J1 = cotask.Task(Joint1.run, name = 'Task1_J1', priority = 2,
+                             period = 10, profile = True, trace = False)
     
-    task2_Enco = cotask.Task (enco1.task, name = 'Task2_Encoder', priority = 3, 
+    task2_J2 = cotask.Task(Joint2.run, name = 'Task1_J2', priority = 2, 
                               period = 10, profile = True, trace = False)
     
-    task3_Cont = cotask.Task (control1.task, name = 'Task3_Controller', priority = 5, 
-                              period = 20, profile = True, trace = False)
+    task3_J3 = cotask.Task(Joint3.run, name = 'Task1_J3', priority = 2, 
+                              period = 10, profile = True, trace = False)
             
-    task4_Mot2 = cotask.Task (motor2.task, name = 'Task4_Motor2', priority = 2,
-                              period = 20, profile = True, trace = False)
+    task4_B = cotask.Task(Brain.run, name = 'Task4_B', priority = 3,
+                              period = 10, profile = True, trace = False)
     
-    task5_Enco2 = cotask.Task (enco2.task, name = 'Task5_Encoder2', priority = 4, 
-                               period = 10, profile = True, trace = False)
-    
-    task6_Cont2 = cotask.Task (control2.task, name = 'Task6_Controller2', priority = 6, 
-                               period = 20, profile = True, trace = False)
-    
-    cotask.task_list.append (task1_Mot)
-    cotask.task_list.append (task2_Enco)
-    cotask.task_list.append (task3_Cont)
-    cotask.task_list.append (task4_Mot2)
-    cotask.task_list.append (task5_Enco2)
-    cotask.task_list.append (task6_Cont2)    
-    
+    cotask.task_list.append(task1_J1)
+    cotask.task_list.append(task2_J2)
+    cotask.task_list.append(task3_J3)
+    cotask.task_list.append(task4_B)
+
     Contperiod = int(input("Set Controller Period: "))
     Contperiod2 = int(input("Set Second Controller Period: "))
     
